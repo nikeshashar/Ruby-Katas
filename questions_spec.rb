d2149ab761220962bb229337b751857758446ae6
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-24 12:19:42 +0100

    17th Test Passed

--- questions_spec.rb
+++ questions_spec.rb
@@ -91,12 +91,12 @@ describe 'the Friday test :)' do
     expect(n).to eq 'different'
   end
 
-  fspecify 'total_of_array' do
+  specify 'total_of_array' do
     n = total_of_array [1, 3, 5, 6, 2, 8]
     expect(n).to eq 25
   end
 
-  specify 'double_array' do
+  fspecify 'double_array' do
     n = double_array [1, 2, 3]
     expect(n).to eq [1, 2, 3, 1, 2, 3]
   end
