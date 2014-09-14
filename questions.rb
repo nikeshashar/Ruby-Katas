efb0fa6ac08b8886e54bc1c675fbb4b7913c477d
nikesh <nikeshashar@gmail.com>
2014-09-14 23:18:37 +0100

    19th Test Passed

--- questions.rb
+++ questions.rb
@@ -1,30 +1,30 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(n)
-	n.select{ |n| n.start_with?("a") }
+  n.select{ |n| n.start_with?("a") }
 end
 #changed array to 'n' and then tested in IRB. Passed n into the method to use .select and then start_with?
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(n)
-	n.select{ |n| n.match(/\A[aeiou]/) }
+  n.select{ |n| n.match(/\A[aeiou]/) }
 end
 #changed array to 'n'. Used .match as start_with? does not accept regexp. 
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(n)
-	n.compact
+  n.compact
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(n)
-	n.delete_if { |a| a == nil || a == false }
+  n.delete_if { |a| a == nil || a == false }
 end
 #used delete_if for the two above and then checked array with a block.
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(n)
-	n.map { |n| n.reverse }
+  n.map { |n| n.reverse }
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
@@ -32,38 +32,38 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice, 
 def every_possible_pairing_of_students(array)
-	array.combination(2).to_a
+  array.combination(2).to_a
 end
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
-	array.drop(3)
+  array.drop(3)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
-	array.unshift(element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
-	array.sort_by { |n| n[-1] }
+  array.sort_by { |n| n[-1] }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-	letter_count = (string.length / 2.0).ceil
-  	string[0, letter_count]
+  letter_count = (string.length / 2.0).ceil
+    string[0, letter_count]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-	-number.abs
+  -number.abs
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -71,7 +71,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-	array.partition { |n| n.even? }
+  array.partition { |n| n.even? }
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -79,41 +79,42 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
-	array == array.reverse 
-	array.select {|n| n == n.reverse }.size
+  array == array.reverse 
+  array.select {|n| n == n.reverse }.size
 end
 
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-	array.min_by{|n| n.length }
+  array.min_by{|n| n.length }
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
-	array.max_by{|n| n.length }
+  array.max_by{|n| n.length }
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
-	array.inject(:+)
+  array.inject(:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
-	array * 2
+  array * 2
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
-	symbol.to_s
+  symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+  (array.inject(:+).to_f / array.size).round
 end
 
 # get all the elements in an array, up until the first element
--- questions_spec.rb
+++ questions_spec.rb
@@ -101,12 +101,12 @@ describe 'the Friday test :)' do
     expect(n).to eq [1, 2, 3, 1, 2, 3]
   end
 
-  fspecify 'turn_symbol_into_string' do
+  specify 'turn_symbol_into_string' do
     n = turn_symbol_into_string :foobar
     expect(n).to eq 'foobar'
   end
 
-  specify 'average_of_array' do
+  fspecify 'average_of_array' do
     n = average_of_array [10, 15, 25]
     expect(n).to eq 17
   end
