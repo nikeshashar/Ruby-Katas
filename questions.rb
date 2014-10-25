c2c7ee71d7e1ce3d65baea876cc7fd058dfc8d9c
nikesh <nikeshashar@gmail.com>
2014-10-25 19:13:01 +0100

    21st test passing

--- questions.rb
+++ questions.rb
@@ -129,6 +129,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
