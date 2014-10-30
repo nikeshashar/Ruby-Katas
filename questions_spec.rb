a350beb7c7e427cd811ef3dff952519bb67b2066
nikesh <nikeshashar@gmail.com>
2014-10-30 22:49:20 +0000

    28th test passing

--- questions_spec.rb
+++ questions_spec.rb
@@ -146,12 +146,12 @@ describe 'the Friday test :)' do
     expect(n).to eq 4
   end
 
-  fspecify 'round_down_number' do
+  specify 'round_down_number' do
     n = round_down_number 4.9
     expect(n).to eq 4
   end
 
-  specify 'format_date_nicely' do
+  fspecify 'format_date_nicely' do
     n = format_date_nicely Time.new(2013, 10, 31)
     expect(n).to eq '31/10/2013'
   end
